6830bcd717adc10e441ab9a086335fbdb7609fa7
mh-rabbi <mhr221official@gmail.com>
2025-05-23 03:57:12 +0600

    Updated:fragment_settings with change photo option pop up and circle croped image set appearance

--- app/src/main/java/com/rideX/ridex/Fragments/SettingsFragment.java
+++ app/src/main/java/com/rideX/ridex/Fragments/SettingsFragment.java
@@ -1,20 +1,66 @@
 package com.rideX.ridex.Fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.rideX.ridex.R;
 
 public class SettingsFragment extends Fragment {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView profileImage;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        profileImage = view.findViewById(R.id.edit_profile_image);
+        Button changePhoto = view.findViewById(R.id.btn_upload_image);
+        changePhoto.setOnClickListener(v -> showImageSelectDialog());
+        return view;
+    }
+
+    private void showImageSelectDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Upload Photo");
+        builder.setMessage("Choose a method to upload your profile picture:");
+
+        builder.setPositiveButton("Choose from Gallery", (dialog, which) -> openGallery());
+
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+        builder.create().show();
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+            profileImage.setImageURI(selectedImageUri);
+            // Load circular image using Glide
+            Glide.with(this)
+                    .load(selectedImageUri)
+                    .circleCrop()
+                    .into(profileImage);
+            // Optional: Upload to Firebase or save to DB here
+        }
     }
 }
